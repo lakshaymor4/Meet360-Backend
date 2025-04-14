4ddce770244feb12c11e54de7ff0710dbdb568fa
lakshaymor4 <117587156+lakshaymor4@users.noreply.github.com>
2025-04-14 16:10:21 +0530

    up req2

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ from app import create_app, socketio
 
 app = create_app('development')  
 
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+# if __name__ == '__main__':
+#     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
